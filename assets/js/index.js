100dc63bd4f3e1c30165754e75a8f44d2cc182c0
Tim-Gourari <tim.gourarilp@gmail.com>
2022-03-04 08:36:59 +0100

    projet

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+let email = "tim-bastien@gmail.com";
+let password = "viaformation";
+
+
+function btn() {
+    let mail = document.getElementById("mail").value;
+    let pass = document.getElementById("pass").value;
+ if (mail == email && pass == password) {
+    window.open('');
+} else {
+    alert('Email ou mot de passe incorect');
+    return false;
+}   
+}
